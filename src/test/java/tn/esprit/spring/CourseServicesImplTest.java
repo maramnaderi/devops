c26dabd4692e80c35f0a25d7d0d925103f9de2d2
fadi-zaghdoud <fadi.zaghdoud@esprit.tn>
2025-04-14 06:58:48 +0100

    changes

--- src/test/java/tn/esprit/spring/CourseServicesImplTest.java
+++ src/test/java/tn/esprit/spring/CourseServicesImplTest.java
@@ -14,7 +14,7 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-public class CourseServicesImplTest {
+class CourseServicesImplTest {
 
     @InjectMocks
     private CourseServicesImpl courseServices;
